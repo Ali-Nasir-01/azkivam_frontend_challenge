8c9a8cc90ed0fcac9b1761b9fb45972b9a75ec0f
nasir <ali.nasir.0104@gmail.com>
2023-11-30 10:33:59 +0330

    feat: fetch products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,4 +2,24 @@
   <div>products</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $axios } = useNuxtApp();
+const appConfig = useAppConfig();
+const action = appConfig.endpoints.PRODUCTS;
+const size: number = 10;
+const page = shallowRef<number>(1);
+
+const fetchProducts = () => {
+  $axios
+    .post(action, null, {
+      params: { size, page: page.value },
+    })
+    .then(({ status, data }) => {})
+    .catch((err) => {})
+    .finally(() => {});
+};
+
+onBeforeMount(() => {
+  fetchProducts();
+});
+</script>
