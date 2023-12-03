3f7bd3171b3da440c1f91ab6bdea29a47acaad15
nasir <ali.nasir.0104@gmail.com>
2023-12-04 02:11:50 +0330

    fix: add w-full class to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto mt-12 mb-10">
     <div class="md:flex md:gap-4">
-      <div class="md:w-[250px]">
+      <div class="md:w-[300px] w-full">
         <filter-bar />
       </div>
       <div class="mt-5 md:mt-0 w-full">
